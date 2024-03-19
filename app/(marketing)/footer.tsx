5473d10a6f5c5720b271dff316210f1350b03f49
Atheshwaran K <athesh85@gmail.com>
2024-03-19 16:44:57 +0530

    first commit

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
       <div className="max-w-screen-lg mx-auto flex items-center justify-center h-full">
-        Made with ❤️ by Zeros and Ones&copy;
+        Made with ❤️ by Zeros and Ones &copy;
       </div>
     </footer>
   );
