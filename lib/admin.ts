f7976bf490cb2477765077cedb4b4aca81b9c5fd
Atheshwaran K <athesh85@gmail.com>
2024-03-19 17:11:57 +0530

    first commit

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,6 +1,10 @@
 import { auth } from "@clerk/nextjs"
 
-const adminIds = ["user_2dtkPXZVjh3ceHY9jxryfMLtWeE"];
+const adminIds = [
+  "user_2dtkPXZVjh3ceHY9jxryfMLtWeE",
+  "user_2duBAkgLLQqm9W1W11NehfleniA",
+  "user_2duB8TfaRlkeAWWEwaR9jsROtqL",
+];
 
 export const isAdmin = () => {
   const { userId } = auth();
